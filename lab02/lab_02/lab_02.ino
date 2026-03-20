486def9e78e1143fdb09486fa3d599232a7a46ea
du-memg-web <1033656652@qq.com>
2026-03-20 13:21:36 +0800

    完成lab02实验

--- lab02/lab_02/lab_02.ino
+++ lab02/lab_02/lab_02.ino
@@ -1,8 +1,6 @@
-// 定义LED引脚
-const int ledPin = 5;  
+const int ledPin = 2; 
 const int ledPin_R = 16; 
-const int ledPin_B = 2; 
-
+const int ledPin_B = 5; 
 void setup() {
   // 初始化串口通信，设置波特率为115200
   Serial.begin(115200);
@@ -20,8 +18,8 @@ void loop() {
   delay(1000);                 // 保持1秒（1000毫秒）
   
   digitalWrite(ledPin, LOW); 
-  digitalWrite(ledPin_R, LOW);
-  digitalWrite(ledPin_B, LOW);  // 熄灭LED
+  digitalWrite(ledPin_R, LOW); 
+  digitalWrite(ledPin_B,LOW);   // 熄灭LED
   Serial.println("LED OFF");   // 串口输出提示
   delay(1000);                 // 保持1秒
 }
